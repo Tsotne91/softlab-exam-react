2cbe7faea82a3e64f717184f35ae8956907321af
Tsotne91 <tsotne91@gmail.com>
2022-02-26 21:11:17 +0400

    date not working

--- src/AddStudentModal.js
+++ src/AddStudentModal.js
@@ -1,19 +1,22 @@
 import {Button, Form, Modal} from "react-bootstrap";
 import {useState} from "react";
+import axios from "axios";
 
 export default function AddStudentModal(props) {
     const initialValues = {
         firstName: "",
         lastName: "",
         personalNumber: "",
-        email: ""
+        email: "",
+        date: ""
     }
 
     const [formValue, setFormValue] = useState(initialValues);
 
     const submitForm = async (e) =>{
         e.preventDefault();
-
+        await axios.post('/students', formValue);
+        props.onHide();
     }
 
     return (
@@ -24,7 +27,7 @@ export default function AddStudentModal(props) {
             centered
         >
             <Modal.Header closeButton />
-                <Form className="mx-3">
+                <Form className="mx-3" onSubmit={submitForm}>
                     <Form.Label>First Name:</Form.Label>
                     <Form.Control type="text" placeholder="Enter First Name"/>
                     <Form.Label>Last Name:</Form.Label>
@@ -33,6 +36,8 @@ export default function AddStudentModal(props) {
                     <Form.Control type="number" placeholder="Enter Personal Number"/>
                     <Form.Label>Email address</Form.Label>
                     <Form.Control type="email" placeholder="Enter email" />
+                    <Form.label>Birth date</Form.label>
+                    <Form.Control type="date" placeholder="Enter birth date"/>
                     <Button variant="primary" type="submit">
                         Submit
                     </Button>
